fbc96cd287261971c7b299dd678eb50b80ac6e5e
Vadim Zavolokin <peyncw@gmail.com>
2016-05-11 05:24:22 +0300

    adedd new homework for softskills

---
+++ Soft Sckills/Презентация/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+$('a[href*=#]').bind("click", function(e){
+var anchor = $(this);
+$('html, body').stop().animate({
+scrollTop: $(anchor.attr('href')).offset().top
+}, 1000);
+e.preventDefault();
+});
+return false;
+});
+and
+
+$(document).ready(function() {
+$('a[href^="#"]').click(function(){
+var el = $(this).attr('href');
+$('body').animate({
+scrollTop: $(el).offset().top}, 2000);
+return false;
+});
+});
